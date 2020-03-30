a36e6d2acf5c0a4bf5f9f6ca8f4e44abf4d637b1
Rida <rida.zammou@student.hu.nl>
2020-03-30 10:19:00 +0200

    First Commit

---
+++ src/main/java/com/weshop/WebschopIAC/WebschopIacApplication.java
@@ -0,0 +1,13 @@
+package com.weshop.WebschopIAC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebschopIacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebschopIacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weshop/WebschopIAC/WebschopIacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weshop.WebschopIAC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebschopIacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
